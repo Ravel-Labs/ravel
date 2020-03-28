4c98df0be0949eb2cf20d5a7ed3422ee770b9854
Dylan Lott <lott.dylan@gmail.com>
2020-03-28 15:08:18 -0600

    Adds skeletons for pages
    - Adds login, signup, home, track, and about page skeletons
    - Gets a rough style for the site going and usable

--- ui/src/router.js
+++ ui/src/router.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import NewQuote from "./views/NewQuote.vue"
+import Login from "./views/Login.vue"
+import Signup from "./views/Signup.vue"
+import Tracks from "./views/Tracks.vue"
 
 Vue.use(Router)
 
@@ -17,7 +20,21 @@ export default new Router({
             path: '/new',
             name: 'new-quote',
             component: NewQuote
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: Signup,
+        },
+        {
+            path: '/tracks',
+            name: 'tracks',
+            component: Tracks,
         }
-
     ]
 })
---
+++ ui/src/views/Signup.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="section">
+    <div class="level">
+      <div class="tile container level-item">
+        <div class="tile is-vertical is-6">
+        <h1 class="is-size-2 has-text-centered">Signup</h1>
+          <div class="tile">
+            <div class="tile is-parent is-vertical">
+              <article class="tile is-child notification is-primary">
+                <div class="field">
+                  <label class="label has-text-white">Email</label>
+                  <div class="control">
+                    <input class="input" v-model="user.username" type="text" placeholder="email">
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label has-text-white">Password</label>
+                  <div class="control">
+                    <input class="input" v-model="user.password" type="password" placeholder="password">
+                  </div>
+                </div>
+
+                <div class="field level-item">
+                  <button class="button is-medium" @click="submit()">Sign up</button>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        username: "",
+        password: "",
+      }
+    }
+  },
+  methods: {
+    submit () {
+      console.log(this.user.username, this.user.password)
+      return
+    }
+  }
+}
+</script>
