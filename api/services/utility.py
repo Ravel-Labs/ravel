eb61ab38aefa68bce24df24e8012e84e784ccaba
User <user@Users-MacBook.local>
2020-04-16 22:57:40 -0400

    email service

---
+++ api/services/utility.py
@@ -0,0 +1,26 @@
+import re
+
+def emailValidator(email):
+    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    if(re.search(regex,email)):  
+        return True     
+    else:  
+        return False
+
+# Update function to alerts
+def passwordValidator(password):
+    if (len(password)<8): 
+        return False
+    elif not re.search(r"[a-z]", password): 
+        return False
+    elif not re.search(r"[A-Z]", password): 
+        return False
+    elif not re.search(r"[0-9]", password): 
+        return False
+    elif not re.search(r"[_@$]", password): 
+        return False
+    elif re.search(r"\s", password): 
+        return False
+    else:
+        return True
+  
