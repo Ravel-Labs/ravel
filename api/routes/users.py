9c890bf43a396e69f33e08f0b5d2cc91061dcd30
User <user@Users-MacBook.local>
2020-04-25 14:55:33 -0400

    add nl

--- api/routes/users.py
+++ api/routes/users.py
@@ -3,6 +3,7 @@ from flask_jwt import jwt_required, current_identity
 from ravel.api import db
 from ravel.api.models.User import User
 from ravel.api.models.apiresponse import APIResponse
+
 users_bp = Blueprint('users_bp', __name__)
 base_users_url = '/api/users'
 
