8f2c2f56bb139ea5642d58a3e521a60bf74d0dfe
User <user@Users-MacBook.local>
2020-05-04 21:35:25 -0400

    queue testing

--- api/queueWorker.py
+++ api/queueWorker.py
@@ -4,9 +4,34 @@ and asynchronous handling.
 """
 import threading
 import queue
-
+import time
+from datetime import datetime
 Q = queue.Queue()
 
+
+
+class Job():
+    """
+        Job class workers will be handling. 
+        
+        Args
+            function: Initiating function for a unit of work.
+            args: Arguments for the worker to use in executing this job function
+            job_start: Used for logging the time of creation 
+            job_end: Used for logging the time of job completion
+    """
+    def __init__(self, function, args):
+        self.function = function
+        self.args = args
+        self.job_start = datetime.now()
+        self.job_end = None
+
+    def wait(self):
+        time.sleep(4)
+
+    def execute(self):
+        self.function(*self.args)
+        time.sleep(5)
 
 def worker():
     """
@@ -15,18 +40,11 @@ def worker():
     while True:
         item = Q.get()
         print(f'Working on {item}')
+        item.execute()
         print(f'Finished {item}')
         Q.task_done()
-
+        if Q.empty:
+            Q.join()
 
 # turn-on the worker thread
 threading.Thread(target=worker, daemon=True).start()
-
-# send thirty task requests to the worker
-for item in range(30):
-    Q.put(item)
-print('All task requests sent\n', end='')
-
-# block until all tasks are done
-Q.join()
-print('All work completed')
