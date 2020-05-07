d6282ae2ad809d3c5ffb5e365abe43bbb7c3c0a5
User <user@Users-MacBook.local>
2020-05-06 20:27:37 -0400

    clearn up

--- api/queueWorker.py
+++ api/queueWorker.py
@@ -2,12 +2,10 @@
 Q creates a Queue that we can push jobs into for processing
 and asynchronous handling.
 """
-import threading
+
 from flaskthreads import AppContextThread
-import queue
-import time
 from datetime import datetime
-from flask import current_app
+import queue
 Q = queue.Queue()
 
 
@@ -21,6 +19,9 @@ class Job():
             job_start: Used for logging the time of creation 
             job_end: Used for logging the time of job completion
     """
+    def __repr__(self):
+        return f"Job started at {self.job_start} : ended {self.job_end}"
+
     def __init__(self, function, args):
         self.function = function
         self.args = args
@@ -39,4 +40,6 @@ def worker():
         print(f'Working on {item}')
         item.execute()
         print(f'Finished {item}, {Q.qsize()} left')
+        setattr(item, 'job_end', datetime.now())
         Q.task_done()
+        
