1d71f814215dc1d16f23cd49d1801b8d4a57bea3
Dylan Lott <lott.dylan@gmail.com>
2020-07-27 15:10:41 -0600

    adds interceptors to axios response middleware for invalid auth handling

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -1,9 +1,26 @@
-
+import router from '@/router';
 import axios from 'axios'
 const ls = window.localStorage
-export default () => axios.create({
+const api = axios.create({
   baseURL: `${process.env.VUE_APP_API_URL}`,
   headers: {
     'Authorization': `JWT ${ls.getItem('token')}`
   }
 })
+
+api.interceptors.response.use((response) => {
+  console.log('axios response: ', response)
+  return Promise.resolve(response) 
+}, (err) => {
+  if (err == "Error: Request failed with status code 401") {
+    // handle missing token or token invalidation error
+    router.push({ name: 'login' })
+    return Promise.reject(err)
+  }
+
+  // handle and log general error
+  console.log('axios interceptor error: ', err)
+  return Promise.reject(err)
+})
+
+export default () => api
