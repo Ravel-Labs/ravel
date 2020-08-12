091b4f41f8bf27536c7e82e943c528f46953f198
Dylan Lott <lott.dylan@gmail.com>
2020-08-12 13:34:25 -0600

    Adds terms of service page and router changes

---
+++ ui/src/views/TermsOfService.vue
@@ -0,0 +1,234 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Ravel Labs Terms of Service</h1>
+      <p>
+      </p>
+      <p>We take the privacy of our users seriously. Your material is not claimed by Ravel Labs, and the intellectual
+        property and copyright of your works remain all yours. The files you upload and the resulting mastered files are
+        not shared with any customers or persons.
+      </p>
+      <p>
+        Thank you for using Ravel Labs (hereinafter also referred to as “we,” “us,” “our,” “the platform,” “the
+        application,” “the service,” “the site,” and “the system”). By using Ravel Labs, you agree to comply with and be
+        bound by the following terms of service, which govern ravelmusic.io, our social media pages, forums, and any
+        other
+        brand-related media created or used by us in the future. If you do not agree to these terms of service then you
+        should not use Ravel Labs.
+      </p>
+      <p>
+        You further agree by using Ravel Labs that you are over the age of 18 or have the express consent of a guardian
+        or
+        parent to use the application if under the age of 18.
+      </p>
+      <p>These terms of service may be altered at any time without notice to you. We’ll post modifications to these
+        terms
+        of service to this page. By continuing to use Ravel Labs, you agree to comply with the most recent version of
+        the
+        terms of service available on the site.
+      </p>
+      <p>Copyright:
+        Ravel Labs and its content, including but not limited to the graphics, visuals, design, codes, algorithms and
+        other site and application related matters are the property of Ravel Labs or its content suppliers and is
+        protected by United States and international copyright laws. You are prohibited from redistributing, copying,
+        publishing or otherwise using any such materials in any manner that violates such laws.
+      </p>
+      <p>Privacy Policy:
+        Our Privacy Policy outlines the information that Ravel Labs collects from you, how it uses that information, and
+        the extent to which it protects that information. By using the site, you agree that we can use such information
+        in
+        accordance with our Privacy Policy.
+      </p>
+      <p>Your Content, Rights and Admittances:
+        By using our site you are provided with a non-transferrable, non-exclusive license to access the platform while
+        upholding our terms of service and other policies. Our platform offers you an opportunity to modify and alter
+        your
+        existing creative material. By using our platform you are acknowledging that this material, which shall be
+        henceforth called your “works,” is your own intellectual property and not the intellectual property of another
+        person or entity. This material is not claimed by Ravel Labs, and the intellectual property and copyright of
+        your
+        works remain yours when using our service. You may not copy, upload or share content that is not your
+        intellectual
+        property unless you have express consent to do so from the party or parties that own the content. Should you
+        violate someone else’s copyright or intellectual property rights when using our service Ravel Labs is not liable
+        for any damages incurred by you for such violations, and by using our platform you give us the right to
+        terminate
+        or suspend your account for said violations.
+      </p>
+      <p>
+        By using our service you accept that Ravel Labs will be hosting, sharing, and modifying your intellectual
+        property
+        for a reasonable amount of time while you are using our platform, and you consent to us and trusted entities we
+        work with to back up and store / share your works with you. We are not responsible for ensuring that the content
+        you share with the platform abides by the aforementioned and forthcoming principles.
+      </p>
+      <p>Using our platform may require the use of personal email verification or authentication, or other means of
+        password encryption to maintain privacy for your account. It is your responsibility to preserve the integrity of
+        this information, and to not share your account or login details with other parties. Should other parties gain
+        access to your account you will be responsible for their use of the account. In a situation where you feel your
+        account or login info has been obtained without your permission you must notify us in writing, and change your
+        password as soon as possible.
+      </p>
+      <p>By using the platform you agree to use it as an individual, with your own intellectual property, and not as an
+        institution or larger entity or group of individuals operating through a single account. Using our platform with
+        a
+        single account meant to service the intellectual property of other people or entities is strictly prohibited,
+        and
+        is in violation of the terms of service agreement.
+      </p>
+      <p>
+        In addition you are permitted to provide links to the application and its contents, provided that those links
+        are
+        not posted to sites that include pornographic, obscene, defamatory, or otherwise illegal / harmful material.
+        This
+        permission is granted unless we request you to cease providing links to our site, upon which you agree to
+        discontinue providing such links immediately.
+      </p>
+      <p>If at any time we feel your account does not abide by the Privacy Policy, Terms of Service, Acceptable Use
+        Policy, the law or other aforementioned policies encompassed by Ravel Labs, or threatens the livelihood,
+        confidentiality or intellectual property rights of other persons and / or entities we reserve the right to
+        terminate your account and / or remove other content you have contributed to the platform. Termination can also
+        result if your activity or account subjects us to legal liability, hampers the service for other users or
+        negatively impacts other users.
+      </p>
+      <p>Proper Acknowledgement for Releases:
+        In the event that the application is used on a song / piece of audio that is released on iTunes, Spotify or
+        other
+        promotional or relevant medium, we ask that you include "Mastered by Ravel Labs.com" in the credits section of
+        said release; however, it is not a requirement.
+      </p>
+      <p>
+        Modification and Termination of the Site
+        Ravel Labs is constantly trying to improve the site. Accordingly, Ravel Labs reserves the right to make changes
+        to
+        its site at any time. We also reserve the right to stop or terminate the site altogether. If we stop or
+        terminate
+        the service, we will give you advance notice to get any information belonging to you out of the site, if
+        providing
+        such notice is reasonably possible.
+      </p>
+      <p>
+        DISCLAIMER OF WARRANTIES:
+        RAVEL LABS’ SERVICES, INCLUDING WITHOUT LIMITATION OUR WEBSITE, APPLICATIONS, FORUM, PLUGINS, SOCIAL MEDIA
+        PAGES,
+        ALL SERVICES PROVIDED THROUGH OR BY OUR WEBSITE OR OTHER APPS, AND ALL INFORMATION, MATERIALS, PRODUCT, CONTENT,
+        AND OTHER SERVICES INCLUDED ON OR OTHERWISE MADE AVAILABLE TO YOU BY RAVEL LABS (HEREINAFTER “THE OFFERINGS”)
+        ARE
+        PROVIDED “AS IS,” “AS AVAILABLE,” WITH “ALL FAULTS,” AND “WITH ALL IMPERFECTIONS”. OTHER THAN AS EXPRESSLY SET
+        FORTH IN THESE TERMS, RAVEL LABS MAKES NO SPECIFIC REPRESENTATIONS OR WARRANTIES OF ANY KIND, EXPRESSED OR
+        IMPLIED, AS TO THE OFFERINGS MADE AVAILABLE TO YOU BY RAVEL LABS. FOR EXAMPLE, RAVEL LABS DOES NOT PROMISE THAT
+        THE SITE WILL BE ERROR FREE, VIRUS FREE AND WITHOUT ISSUE, THAT ITS SECURITY WILL BE IMPENETRABLE, THAT YOU WILL
+        HAVE CONTINUOUS OPERATION OF OR ACCESS TO THE SITE, NOR THAT IT IS RESPONSIBLE FOR THE STORAGE AND SAFEKEEPING
+        OF
+        YOUR WORKS.
+      </p>
+      <p>
+        TO THE EXTENT PERMITTED BY APPLICABLE LAW, RAVEL LABS EXPRESSLY DISCLAIMS ALL WARRANTIES, INCLUDING BUT NOT
+        LIMITED TO THE IMPLIED WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, COMPATIBILITY AND
+        NON-INFRINGEMENT.
+      </p>
+      <p>LIMITATION OF LIABILITY FOR USE OF OUR SITE
+        TO THE EXTENT PERMITTED BY APPLICABLE LAW, RAVEL LABS AND ITS AFFILIATED PARTIES ARE NOT LIABLE, AND YOU AGREE
+        TO
+        NOT HOLD US RESPONSIBLE, FOR ANY DAMAGES OR LOSSES (INCLUDING BUT NOT LIMITED TO LOST PROFITS OR REVENUE,
+        GOODWILL
+        OR REPUTATION, DATA LOSSES, FINANCIAL LOSSES, OR CONSEQUENTIAL, INDIRECT, EXEMPLARY, SPECIAL, OR PUNITIVE
+        DAMAGES)
+        RESULTING DIRECTLY OR INDIRECTLY FROM YOUR USE OF THE SITE, OR YOUR INABILITY TO USE THE SITE. YOU EXPRESSLY
+        AGREE
+        THAT YOUR USE OF RAVEL LABS IS AT YOUR SOLE RISK.
+
+      </p>
+      <p>REGARDLESS OF THE PREVIOUS PARAGRAPH, IF RAVEL LABS OR ITS’ AFFILIATED PARTIES ARE FOUND TO BE LIABLE, OUR
+        TOTAL
+        LIABILITY TO YOU OR ANY THIRD PARTY IS LIMITED TO THE AMOUNT THAT YOU PAID TO USE THE SITE.
+      </p>
+      <p>INDEMNIFICATION
+        By using our service you agree to defend, indemnify, and hold harmless Ravel Labs and its’ subsidiaries, agents,
+        employees, successors, related parties, advertisers, directors, advisors, shareholders, officers and affiliates
+        (collectively, “Affiliated Parties”) against and from any and all claims, demands, costs, expenses, liabilities,
+        and damages, including legal fees, arising from your use of the site, a violation by you of these terms of
+        service
+        or other policies, and / or any intellectual property infringement or violation made by you or your account on
+        the
+        site.
+      </p>
+      <p>
+        Third Party Disclaimer
+        By using our platform you understand that that material and opinions shared by third party sources on our
+        platform
+        are not representative of Ravel Labs and its affiliates. This third party content then does not represent Ravel
+        Labs nor does it reflect our opinions. Any third party content shared on our platform represents solely the
+        thoughts of the original creator and is in no way endorsed by us.
+      </p>
+      <p>
+        In the Case of Unlawful Practices:
+        If at any time we feel that any aspects of this agreement are being violated we reserve the right to investigate
+        violations or actions and take whatever actions we see fit. These include but are not limited to disclosing
+        information that we obtain from you on the platform to governmental and law enforcement agencies.
+      </p>
+      <p>Compliance with Legal Matters:
+        By using Ravel Labs you agree to abide by and comply with ordinances, laws (both foreign and domestic), statutes
+        and regulations that govern your use of our platform and the material and contents provided within it.
+      </p>
+      <p>Disputes
+        You and Ravel Labs each agree that any claim or dispute related in any way to these terms of service or to your
+        use of the site will be resolved exclusively by final and binding arbitration, rather than in court. The
+        arbitration will be conducted by the American Arbitration Association ("AAA") under its rules and procedures.
+        The
+        arbitration proceedings will be conducted only on an individual basis and not in a consolidated, class or
+        representative action. The arbitration proceedings will be held in Atlanta, Georgia and each party will share
+        the
+        cost for the arbitration.
+      </p>
+      <p>If for any reason the claim or dispute proceeds to court rather than arbitration, then you and Ravel Labs each
+        agree to resolve the claim or dispute in state or federal court in Atlanta, Georgia; to submit to the personal
+        jurisdiction of said court; and to waive any right to a jury trial.
+      </p>
+      <p>Regardless of the foregoing paragraphs, you and Ravel Labs may bring suit in court to enjoin infringement or
+        other misuse of intellectual property rights.
+      </p>
+      <p>Applicable Law
+        By using the site, you agree that, except to the extent inconsistent or preempted by federal law, the laws of
+        the
+        state of California, without regard to principles of conflict of laws, will govern the Terms of Service and any
+        claim or dispute of any kind that has arisen or may arise between you and Ravel Labs.
+
+      </p>
+
+      <p>Severability
+        If any of the above terms of service are deemed unenforceable, void, or invalid, that terms shall be deemed
+        severable and shall not affect the enforceability and validity of any remaining terms of service.
+
+      </p>
+      <p>General
+        No aspect of this agreement constitutes a partnership or joint venture of any kind between you and Ravel Labs,
+        its
+        subsidiaries, agents, employees, relevant parties, directors, advisors, shareholders, officers and / or
+        affiliates.
+      </p>
+      <p>PRICING TERMS AND CONDITIONS
+        Individual Account Use and Declaration:
+        Accounts created and used within the platform are available solely for individual use. They are restricted to
+        single individuals, and cannot be used by other people, entities or members of an institution that purchases an
+        account.
+      </p>
+      <p>
+        Updates to the software will be free unless explicitly outlined, and will be supported for up to three years
+        from
+        the date of purchase.
+      </p>
+
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'TermsOfService'
+  }
+</script>
+
+<style>
+
+</style>
