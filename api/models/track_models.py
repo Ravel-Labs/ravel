7655a6828f1ac5975b19ecc4d055f68f49fb8dd5
Dylan Lott <dylan@storj.io>
2020-10-10 10:52:57 -0700

    updates track and trackout models with uuids (#147)

--- api/models/track_models.py
+++ api/models/track_models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from api import db
+import uuid
 
 '''
     This file contains the database relational schema
@@ -19,6 +20,7 @@ class Track(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     trackouts = db.relationship('TrackOut', backref=db.backref('trackouts', passive_deletes=True), lazy='dynamic')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    uuid = db.Column(db.String, default=uuid.uuid4)
 
     '''
         Configurable Fields
@@ -35,6 +37,7 @@ class Track(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
+            "uuid": self.uuid,
             "name": self.name,
             "user_id": self.user_id,
             "artist": self.artist,
@@ -52,8 +55,9 @@ class TrackOut(db.Model):
         Database Generated Fields
     '''
     id = db.Column(db.Integer, primary_key=True)
-    track_id = db.Column(db.Integer, db.ForeignKey('track.id', ondelete='CASCADE'))
+    track_id = db.Column(db.Integer, db.ForeignKey('track.uuid', ondelete='CASCADE'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    uuid = db.Column(db.String, default=uuid.uuid4)
 
     '''
         Configurable Fields
@@ -76,6 +80,7 @@ class TrackOut(db.Model):
     def to_dict(self):
         trackout = {
             "id": self.id,
+            "uuid": self.uuid,
             "user_id": self.user_id,
             "track_id": self.track_id,
             "created_at": self.created_at,
@@ -88,12 +93,19 @@ class TrackOut(db.Model):
         return trackout
 
 
+'''
+    FX Models 
+
+    These are models tracked for individual FX that are applied to tracks. 
+    They're created to record calculated TrackOut parameters and values.
+    They are currently only used for internal processing.
+'''
 class Equalizer(db.Model):
     '''
         Database Generated Fields
     '''
     id = db.Column(db.Integer, primary_key=True)
-    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.id", ondelete='CASCADE'))
+    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.uuid", ondelete='CASCADE'))
 
     '''
         Configurable Fields
@@ -118,7 +130,7 @@ class Deesser(db.Model):
         Database Generated Fields
     '''
     id = db.Column(db.Integer, primary_key=True)
-    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.id", ondelete='CASCADE'))
+    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.uuid", ondelete='CASCADE'))
 
     '''
         Configurable Fields
@@ -139,7 +151,7 @@ class Reverb(db.Model):
         Database Generated Fields
     '''
     id = db.Column(db.Integer, primary_key=True)
-    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.id", ondelete='CASCADE'))
+    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.uuid", ondelete='CASCADE'))
 
     '''
         Configurable Fields
@@ -158,7 +170,7 @@ class Compressor(db.Model):
         Database Generated Fields
     '''
     id = db.Column(db.Integer, primary_key=True)
-    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.id", ondelete='CASCADE'))
+    trackout_id = db.Column(db.Integer, db.ForeignKey("track_out.uuid", ondelete='CASCADE'))
 
     '''
         Configurable Fields
