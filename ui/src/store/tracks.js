ed1e0028d9e33099ef2973a2500dc0d43c1b30f5
Dylan Lott <lott.dylan@gmail.com>
2020-10-31 15:41:47 -0600

    fixes trackouts upload in gui to use uuids instead of ids

--- ui/src/store/tracks.js
+++ ui/src/store/tracks.js
@@ -247,16 +247,15 @@ const tracks = {
         }
       },
 
-      async getWavefile ({ commit }, trackID) {
-        try {
-         let { data } = await API().get(`/tracks/wav/${trackID}`)
-         console.log('got wavfile response: ', data)
-         return data
-        } catch (err) {
-          console.log('error getting wavefile: ', err) 
-          return err
-        }
-      },
+      // async getWavefile ({ commit }, trackID) {
+      //   try {
+      //    let { data } = await API().get(`/tracks/wav/${trackID}`)
+      //    return data
+      //   } catch (err) {
+      //     console.log('error getting wavefile: ', err) 
+      //     return err
+      //   }
+      // },
       async createTrackoutWithoutWav ({ commit }, trackout) {
         try {
           let payload = {
@@ -278,7 +277,7 @@ const tracks = {
       // updateTrackoutWithWav takes a payload of `{ id: <id>, formData: {}}`
       async updateTrackoutWithWav ({ commit }, payload) {
         try {
-          let { data } = await API().put(`/trackouts/wav/${payload.id}`,
+          let { data } = await API().put(`/trackouts/wav/${payload.uuid}`,
             payload.formData,
             {
               headers: {
@@ -286,7 +285,6 @@ const tracks = {
               }
             } 
           )
-          console.log('uploaded successfully: ', data)
           commit('UPDATE_TRACKOUT_WAV_SUCCESS', data)
           return Promise.resolve(data)
         } catch (err) {
